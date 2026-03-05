bb28f145aaf52cba701bdce6fa695fe79b0b993d
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2026-03-04 18:56:37 -0800

    move terraform to cloud repo

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-# ──────────────────────────────────────────────────────────────────────
-# Cloud Agent module
-# ──────────────────────────────────────────────────────────────────────
-
-resource "google_service_account" "cloud_agent" {
-  account_id   = "cloud-agent"
-  display_name = "Cloud Agent (WebSocket brain server)"
-}
